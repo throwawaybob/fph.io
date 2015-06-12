17308acca5f32b9727dc69b064fd8947e85c1b40
Benedict Lewis <benedict@ovalbit.com>
2015-06-12 01:32:31 +0100

    Added GA

--- packages/fph/lib/client/templates/custom_post_author.js
+++ packages/fph/lib/client/templates/custom_post_author.js
@@ -1,6 +1,5 @@
 Template.registerHelper('adminFlair', function(user) {
   var user = (typeof user === 'string') ? Meteor.users.findOne(user) :  user;
-
   if (user && user.isAdmin === true) {
     return '<span class="admin-flair">[A]</span>';
   }
